dc0a1ce270b87c19258a832ac42c71c15f3a96ed
wnhlee <2wheeh@gmail.com>
2024-03-11 12:39:31 +0900

    add FixedEditorRefPlugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
-import { InitialConfigType, LexicalComposer } from '@lexical/react/LexicalComposer';
 import { EditorRefPlugin } from '@lexical/react/LexicalEditorRefPlugin';
+// import { FixedEditorRefPlugin } from './FixedEditorRefPlugin';
 
+import { InitialConfigType, LexicalComposer } from '@lexical/react/LexicalComposer';
 import { EditorRefProvider, useEditorRefContext } from './useEditorRefContext';
 import { OutSide } from './Outside';
 
 function Editor() {
   const { onRef } = useEditorRefContext() ?? {};
 
-  return <>{onRef !== undefined && <EditorRefPlugin editorRef={onRef} />}</>;
+  return (
+    <>
+      {onRef !== undefined && <EditorRefPlugin editorRef={onRef} />}
+      {/* {onRef !== undefined && <FixedEditorRefPlugin editorRef={onRef} />} */}
+    </>
+  );
 }
 
 function App() {
   const initialConfig: InitialConfigType = {
-    namespace: '',
+    namespace: 'hello',
     onError: error => {
       throw error;
     },
